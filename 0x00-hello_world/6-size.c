82170ea256fbc8cadcd7d91ec33d50b775911f16
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-18 23:25:47 +0000

    fix: corrected output formating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
-int main(void){
-
-    printf("Size of a char: %lu bytes\n", sizeof(char));
-    printf("Size of an int: %lu bytes\n", sizeof(int));
-    printf("Size of a long int: %lu bytes\n", sizeof(long int));
-    printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
-    printf("Size of a float: %lu bytes\n", sizeof(float));
+/**
+ *  main - print size of types
+ *  Return: 0 means success
+ */
+int main(void)
+{
+    printf("Size of a char: %d byte(s)\n", sizeof(char));
+    printf("Size of an int: %d byte(s)\n", sizeof(int));
+    printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %d byte(s)\n", sizeof(float));
+   
     return 0;
 }
 
