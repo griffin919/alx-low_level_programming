b9590dc046abffe516db626dd0f521a971601d10
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-19 09:15:45 +0000

    feat: print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* more headers goes here */
+
+/**
+ * main - printing lowercase alphabet
+ *
+ * Return: 0 this returns zero
+*/
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
