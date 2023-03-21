1ba60877030eaaf7568f6b3c9d565fbffbc59b99
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-21 21:34:53 +0000

    feat: a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: 1 means success
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
