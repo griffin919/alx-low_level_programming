b0878284493af6840e901e64b78d97247b8844ab
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-21 19:12:07 +0000

    feat: a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  main: a program that prints _putchar,
+ *  followed by a new line.
+ *
+ *  Return: 0 means success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
