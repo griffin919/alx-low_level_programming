791c4d6eaba88fb6dd6ccb29f710eed6089b9587
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-21 22:06:19 +0000

    fix: corrected betty linter error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,17 @@
 
 /**
  * print_alphabet - prints alphabet in lowercase
- * @c: The character to print
- * 
+ * The character to print
  * Return: void
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for(i = 'a'; i <= 'z'; ++i)
+	for (i = 'a'; i <= 'z'; ++i)
 	{
 		_putchar(i);
 	}
-	_putchar(i);
+	_putchar('\n');
 
 }
