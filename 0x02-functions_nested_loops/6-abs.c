f00cf72fbfbb7ddb94ee710f942b10665ffeed8f
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-22 02:09:58 +0000

    feat: a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,16 +6,14 @@
 #include <ctype.h>
 
 /**
- * _abs - a function that computes the 
+ * _abs - a function that computes the
  *  absolute value of an integer.
  *
  * @c: abs arg
  * Return: 1 true 0 means false
  */
 int _abs(int c)
-{	
-
+{
 	return abs(c);
-
 }
 #endif
