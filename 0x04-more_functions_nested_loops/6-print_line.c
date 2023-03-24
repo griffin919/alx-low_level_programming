e0249b9bfee88e84eb48902e97656a70b5785e45
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-24 16:23:21 +0000

    feat: a function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line -  a function that draws a
+ * straight line in the terminal.
+ *
+ * @n : number of time _ should be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		 _putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
