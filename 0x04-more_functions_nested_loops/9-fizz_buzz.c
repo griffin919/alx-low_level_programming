23e407ead1fe43923bec46650a3d5708eed62127
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-24 18:31:14 +0000

    feat: The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - The “Fizz-Buzz test”
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			 printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			if (i % 3 == 0)
+				printf("%s ", "Fizz");
+
+			else if (i % 5 == 0)
+				printf("%s ", "Buzz");
+
+			else
+				printf("%d ", i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
