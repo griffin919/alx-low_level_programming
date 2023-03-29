ae48cb5d95267a5ab89fe263828a5365d571944d
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-29 20:59:14 +0000

    feat: 2. strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ *
+ * @s1 : string 1
+ * @s2 : string 2
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+
+}
