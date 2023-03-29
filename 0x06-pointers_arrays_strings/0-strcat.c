26698fb729229b447c379e20bbee86834f1acd0e
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-29 19:31:51 +0000

    feat: 0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ *
+ * @dest : destination var
+ * @src : source var
+ *
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
