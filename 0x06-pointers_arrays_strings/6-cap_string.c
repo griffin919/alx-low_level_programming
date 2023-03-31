6ef7965327a78e463bee559fa842073a60f30564
Prince Boateng <princefiifiboateng@gmail.com>
2023-03-31 06:16:03 +0000

    fix: replaced pointer loop

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,57 +8,33 @@
  *
  * @str : passed in string
  *
- * Return: integer
+ * Return: pointer
  */
 char *cap_string(char *str)
 {
-
-/*
-	char *ptr = str;
-
-	while (*ptr != '\0')
-	{
-		if (*ptr == ' ' || *ptr == '.' || *ptr == '-' || *ptr == '\n')
-		{
-			ptr++;
-			*ptr = toupper(*ptr);
-		}
-		else if (*ptr == '\t')
-		{
-			*ptr = ' ';
-		}
-		ptr++;
-	}
-
-	return (str);
-
-*/
-
 	int len = strlen(str);
 	int i = 0;
 
-
-	for(i =0; i <= len; i++)
-	{	
-		if (str[i] != '\0' )
-		{	
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] != '\0')
+		{
 			if (str[i] == '\t')
 			{
 				str[i] = ' ';
 			}
 		}
-	
 	}
 
-	for(i =0; i <= len; i++)
+	for (i = 0; i <= len; i++)
 	{
-		if (str[i] != '\0' )
+		if (str[i] != '\0')
 		{
 			if (str[i] == ' ' || str[i] == '.' || str[i] == '\n')
-                        {
-				str[i + 1] = toupper(str[i +1]);
-			}												                 }
+			{
+				str[i + 1] = toupper(str[i + 1]);
+			}
+		}
 	}
-	return str;
+	return (str);
 }
-
