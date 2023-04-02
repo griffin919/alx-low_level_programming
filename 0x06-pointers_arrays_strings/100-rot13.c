fa3e51a36f375fa1b7d4616f13e4c5a552335bd4
Prince Boateng <princefiifiboateng@gmail.com>
2023-04-02 19:46:06 +0000

    feat: a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ *  rot13 - encodes passed string with RTO13
+ *
+ *  @str: passed string
+ *
+ *  Return: encoded string
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+
+	char alphax2[52] = "abcdefjhijklmnopqrstuvwxyzabcdefjhijklmnopqrstuvwxyz";
+	int alpha_index, i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		if (ptr[i] >= 'A' && ptr[i] <= 'Z')
+		{
+			for (alpha_index = 0; alpha_index < 26; alpha_index++)
+			{
+				char letter = 'a' + alpha_index;
+
+				if (ptr[i] == letter)
+				{
+					int offset_index = alpha_index + 13;
+
+					ptr[i] = alphax2[offset_index];
+					break;
+				}
+			}
+		}
+		if (ptr[i] >= 'a' && ptr[i] <= 'z')
+		{
+			for (alpha_index = 0; alpha_index < 26; alpha_index++)
+			{
+				char letter = 'a' + alpha_index;
+
+				if (ptr[i] == letter)
+				{
+					int offset_index = alpha_index + 13;
+
+					ptr[i] = alphax2[offset_index];
+					break;
+				}
+			}
+		}
+	}
+	return (str);
+}
