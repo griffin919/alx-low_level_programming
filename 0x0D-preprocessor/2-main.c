891ac1d4f7afec2ef16839d8cb9199512dc92132
Prince Boateng <princefiifiboateng@gmail.com>
2023-04-17 15:56:41 +0000

    feat:File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name file was compiled from.
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
