2f611a3ae89501091bb2e50a61c5d6c77235b5cb
Prince Boateng <princefiifiboateng@gmail.com>
2023-04-18 21:39:26 +0000

    a function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - struct for dog
+ *
+ * @name: first member / pointer variable
+ * @age: second member
+ * @owner: pointer variable
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
