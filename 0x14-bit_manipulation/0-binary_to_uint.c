f9696bab952ca56874a21eda4b53ece252278ea6
Prince Boateng <princefiifiboateng@gmail.com>
2023-07-07 17:57:53 +0000

    Task0.0: convert binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * power - custom function to calc exponents
+ * @base: :)
+ * @exponent: :)
+ * Return: result
+ */
+unsigned int power(int base, int exponent)
+{
+	unsigned int result = 1;
+	int i;
+
+	for (i = 0; i < exponent; i++)
+	{
+		result *= base;
+	}
+
+	return (result);
+}
+
+/**
+ * binary_to_uint - convert binary to positive int
+ * @b: binary char array/string
+ * Return: converted unsigned int
+ */
+unsigned int binary_to_uint (const char *b)
+{
+	int len = 0, j, k;
+	unsigned int convertedTot = 0;
+
+	while (b[len] != '\0')
+	{
+		if ((b[len] != '1' && b[len] != '0') || b == NULL)
+			return (0);
+		len++;
+	}
+
+	for (j = 0, k = len; j < len || k >= 0; j++, k--)
+	{
+		if (b[j] == '1')
+			convertedTot += 1 * power(2, k - 1);
+	}
+
+	return (convertedTot);
+}
